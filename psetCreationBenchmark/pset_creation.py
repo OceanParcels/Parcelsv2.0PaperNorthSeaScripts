6666f864d2347e5476952bcfd9e502e76af7f2d6
delandmeter <philippe.delandmeter@gmail.com>
2018-03-20 18:05:43 +0100

    We don't create a pset anymore, we simply create many particles (this is the expensive part)

--- psetCreationBenchmark/pset_creation.py
+++ psetCreationBenchmark/pset_creation.py
@@ -17,23 +17,41 @@ def fieldset(xdim=40, ydim=100):
     return FieldSet.from_data(data, dimensions)
 
 
+class Obj(object):
+    def __init__(self):
+        self.i = 4
+
+#@profile
 def test_pset_create_lon_lat(fieldset, mode, npart=100):
     lon = np.linspace(0, 1, npart, dtype=np.float32)
     lat = np.linspace(1, 0, npart, dtype=np.float32)
-    pset = ParticleSet(fieldset, lon=lon, lat=lat, pclass=ptype[mode])
+    #pset = ParticleSet(fieldset, lon=lon, lat=lat, pclass=ptype[mode])
+    for i in range(npart):
+        p = JITParticle(0,0,fieldset=fieldset, depth=0, cptr=None, time=0)
+        #p = Obj()
 
-npart = 10000
+npart = 80000
 
 timer.root = timer.Timer('Main')
 timer.fieldset = timer.Timer('FieldSet', parent=timer.root)
 fset = fieldset()
 timer.fieldset.stop()
 timer.pset = timer.Timer('ParticleSet', parent=timer.root)
+timer.JITP = timer.Timer('JITParticle', parent=timer.pset, start=False)
+timer.cptr = timer.Timer('cptr', parent=timer.JITP, start=False)
+timer.super = timer.Timer('super', parent=timer.JITP, start=False)
+timer.gridIndex = timer.Timer('GridIndex', parent=timer.JITP, start=False)
+timer.gobj = timer.Timer('GridIndex obj', parent=timer.gridIndex, start=False)
+timer.gobjptr = timer.Timer('GridIndex obj ptr', parent=timer.gridIndex, start=False)
+timer.gobjptr_1 = timer.Timer('GridIndex obj ptr 1', parent=timer.gobjptr, start=False)
+timer.gobjptr_2 = timer.Timer('GridIndex obj ptr 2', parent=timer.gobjptr, start=False)
+timer.gobjptr_3 = timer.Timer('GridIndex obj ptr 3', parent=timer.gobjptr, start=False)
+timer.gobjptrval = timer.Timer('GridIndex obj ptr value', parent=timer.gridIndex, start=False)
 test_pset_create_lon_lat(fset, 'jit', npart=npart)
 timer.pset.stop()
 timer.root.stop()
 
 print('Creating %d particles' % npart)
 #timer.root.print_tree()
-timer.pset.print_local()
+timer.pset.print_tree()
 
