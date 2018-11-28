f2e214e1318a9e1432bda4dc2499536cf881f6b1
delandmeter <philippe.delandmeter@gmail.com>
2018-11-28 14:04:00 +0100

    adding call file

--- run_northsea_mp.py
+++ run_northsea_mp.py
@@ -97,7 +97,3 @@ def run_northsea_mp(outfile, nemo_res='0083'):
         print('running %d / %d [time %g s]: %d particles ' % (day, ndays, timelib.time()-tic, len(pset)))
         pset.execute(kernel, runtime=delta(days=2), dt=900, verbose_progress=False, recovery={ErrorCode.ErrorOutOfBounds: DeleteParticle})
         pfile.write(pset, pset[0].time)
-
-outfile = '/scratch/shared/delandmeter/northSea_plastic/test.nc'
-run_northsea_mp(outfile)
-
