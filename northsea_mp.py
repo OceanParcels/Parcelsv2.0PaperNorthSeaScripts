3e96e852df5e6bc1d6cfd1040a18f4c0d8231abd
delandmeter <philippe.delandmeter@gmail.com>
2018-11-28 16:04:00 +0100

    add nested cmems

--- northsea_mp.py
+++ northsea_mp.py
@@ -1,4 +1,4 @@
 from run_northsea_mp import run_northsea_mp
 
 outfile = '/scratch/shared/delandmeter/northSea_plastic/test.nc'
-run_northsea_mp(outfile)
+run_northsea_mp(outfile, nemo_res='0083', cmems=True)
